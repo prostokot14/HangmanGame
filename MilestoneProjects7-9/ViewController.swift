ca6e23e4646610781ad2d9304693b3fafbf049ea
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2023-06-09 15:02:03 +0200

    Initial Commit

---
+++ MilestoneProjects7-9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneProjects7-9
+//
+//  Created by Антон Кашников on 09.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
